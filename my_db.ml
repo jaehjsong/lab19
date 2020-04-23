40aefc2737d50f47a8f50bec693988dbc57ce392
James Song <song@jamess-air-2.home>
2020-04-23 03:17:34 -0700

    got a database

---
+++ my_db.ml
@@ -0,0 +1,29 @@
+type id = int ;;
+let dbsize = 100 ;;
+
+let db : (int, (string * int)) Hashtbl.t =
+  Hashtbl.create dbsize ;;
+
+let create (id : id) (name : string) : unit =
+  Hashtbl.add db id (name, 0) ;;
+
+let exists (id : id) : bool =
+  Hashtbl.mem db id ;;
+
+let balance (id : id) : int =
+  snd (Hashtble.find db id) ;;
+
+let name (id : id) : int =
+  fst (Hashtble.find db id) ;;
+
+let update (id : id) (bal : int) : unit =
+  let nam = name id in
+  Hashtbl.replace db id (nam, bal) ;;
+
+let close (id : id) : unit =
+  Hashtbl.remove db id ;;
+
+let dump =
+  db
+  |> Hashtbl.iter (fun i (nam, bal) ->
+      Printf.printf "[%d] %s -> %d\n" i nam bal) ;;
